cb24218cba780dbaf99486678269e760640d645c
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-29 21:38:07 +0100

    fixing output

--- vqa/run.sh
+++ vqa/run.sh
@@ -10,6 +10,5 @@ fi
 RUN_NAME="run_11_general"
 QUANTITY="10K"
 
-python convert_tsv.py ${OUTPUT_PATH}/${RUN_NAME}/test_${RUN_NAME}_${QUANTITY}.json ./${RUN_NAME}_${QUANTITY}.tsv \
-    --output_tsv ${OUTPUT_PATH}/${RUN_NAME} \
+python convert_tsv.py ${OUTPUT_PATH}/${RUN_NAME}/test_${RUN_NAME}_${QUANTITY}.json ${OUTPUT_PATH}/${RUN_NAME}/${RUN_NAME}_${QUANTITY}.tsv \
     --image-root ${IMAGE_ROOT}
