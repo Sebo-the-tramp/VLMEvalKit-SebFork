e1b5f3c58bb54f6c2235e31bf260cb4a920d0201
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-30 22:21:16 +0100

    update sbatch

--- small_eval_run_slurm.sh
+++ small_eval_run_slurm.sh
@@ -12,7 +12,7 @@ curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
      -d chat_id="${TELEGRAM_CHAT_ID}" \
      --data-urlencode text="🚀 GPU session started for GENERAL_SMALL_MODELS on $(hostname) at $(date)" >/dev/null &
 
-source /mnt/proj1/eu-25-92/physbench/.venv/bin/activate
+source /home/it4i-thvu/seb_dev/computational_physics/VLMEvalKit-SebFork/.venv/bin/activate
 
 RUN_NAME="run_11_general"
 QUANTITY="10K"
